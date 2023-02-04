70dc831a468c24ca8f712968b7194955c328d29a
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2023-02-04 00:46:18 +0000

    Deploying to gh-pages from @ xottabbu4/goit-react-hw-08-phonebook@353a0a92c6d946c9b41c4971d0e5c7245e2f32b0 🚀

--- static/js/544.bad44110.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184),i={container:{display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function o(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Your personal phonebook"})})}}}]);
-//# sourceMappingURL=544.bad44110.chunk.js.map
