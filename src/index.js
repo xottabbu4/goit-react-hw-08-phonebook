48ad108d9abe4571d8b1e17467266ed23f986b81
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2023-02-06 22:48:53 +0200

    changed input's name

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook" >
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
